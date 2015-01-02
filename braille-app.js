a6e753065c50f0f66bb4d3b29254792e665841b8
Sophie L'Ange <sophie.jeanne.la.douce@gmail.com>
2015-01-01 22:19:33 -0800

    Now you can type something.

--- braille-app.js
+++ braille-app.js
@@ -22,29 +22,146 @@ angular.module("brailleapp", [])
         // Inheritance in JavaScript is a godawful mess. Don't do it.
 
         $scope.data = {
-            title: "Hello Mr. Racette", // Anything put on $scope can be seen by the HTML file.
+            title: "Type Something", // Anything put on $scope can be seen by the HTML file.
             // is really brain dead. I'm setting the dots with the value in source="123456" where each character can
             // be B (black), W (white), R (red) or G (green).
-            dots: [
-            'BWBBWW',  // H
-            'BWWBWW',  // E
-            'BWBWBW',  // L
-            'BWBWBW',  // L
-            'BWWBBW',  // O
-            'WWWWWW',  //
-            'GGWWGW',  // M
-            'GWGGGW',  // R
-            'WWGGWG',  // .
-            'WWWWWW',  //
-            'RWRRRW',  // R
-            'RWWWWW',  // A
-            'RRWWWW',  // C
-            'RWWRWW',  // E
-            'WRRRRW',  // T
-            'WRRRRW',  // T
-            'RWWRWW'   // E
-        ]
+            text: "",
+            dots: []
         };
+
+        $scope.clear = function () {
+            $scope.data.text = "";
+            $scope.data.dots = [];
+        };
+
+        var convert = {
+            A: '100000',
+            B: '110000',
+            C: '100100',
+            D: '100110',
+            E: '100010',
+            F: '110100',
+            G: '110110',
+            H: '110010',
+            I: '010100',
+            J: '010110',
+            "!": "011010",
+            "'": "001000",
+            "-": "001001",
+            ".": "010011",
+            ",": "010000",
+            "?": "011001",
+            " ": "000000"
+        };
+
+        function addBlackDots(binary) {
+            // So it looks like I didn't do the display right.
+            // http://braillebug.afb.org/braille_deciphering.asp
+            // That page says that they're numbered going down column-wise rather than row-wise.
+            // So here we convert from binary to the way that the directive wants it.
+            var dots = "";
+            dots += binary[0] === "1" ? "B" : "W";
+            dots += binary[3] === "1" ? "B" : "W";
+            dots += binary[1] === "1" ? "B" : "W";
+            dots += binary[4] === "1" ? "B" : "W";
+            dots += binary[2] === "1" ? "B" : "W";
+            dots += binary[5] === "1" ? "B" : "W";
+
+            $scope.data.dots.push(dots);
+        }
+
+        function makeCapital (letter) {
+            // If it's a capital letter then add the capital letter sign.
+            // The rules on http://braillebug.afb.org/braille_deciphering.asp
+            // say to do differently if the word is all capital letters
+            // but I'm being a lazy ass and not doing that.
+            if ("A"  <= letter && letter <= "Z") {
+                addBlackDots("000001");
+            }
+        }
+
+        function textToBraille() {
+            $scope.data.dots = [];
+            var text = $scope.data.text;
+            if (!text) {
+                return;
+            }
+
+            var numberMode = false;
+            var code;
+            for (var i = 0; i < text.length; i++) {
+                var result = "";
+                var letter = text[i];
+                var capital = letter.toUpperCase();
+
+                if (capital === "W") {
+                    // W is special since it's not really used in French.
+                    numberMode = false;
+                    makeCapital(letter);
+                    addBlackDots("010111");
+                } else if ("A" <= capital && capital <= "J") {
+                    // So the core set of letters is A to J.
+                    numberMode = false;
+                    makeCapital(letter);
+                    addBlackDots(convert[capital]);
+                } else if ("K" <= capital && capital <= "T") {
+                    // K through T is like A to J but with dot 2 set also.
+                    numberMode = false;
+                    makeCapital(letter);
+                    code = String.fromCharCode(capital.charCodeAt(0) - 10);
+                    result = convert[code].split("");
+                    result[2] = "1";
+                    addBlackDots(result.join(""));
+                } else if (capital === "U" || capital === "V") {
+                    // U and V are like A and B but with bottom dots.
+                    // (If you leave W out then U to Z work like K to T, but there's the W thing.)
+                    numberMode = false;
+                    makeCapital(letter);
+                    code = String.fromCharCode(capital.charCodeAt(0) - 20);
+                    result = convert[code].split("");
+                    result[2] = "1";
+                    result[5] = "1";
+                    addBlackDots(result.join(""));
+                } else if ("X" <= capital && capital <= "Z") {
+                    // X, Y and Z are kind of a mess because of W.
+                    // (If you leave W out then U to Z work like K to T, but there's the W thing.)
+                    numberMode = false;
+                    makeCapital(letter);
+                    code = String.fromCharCode(capital.charCodeAt(0) - 21);
+                    result = convert[code].split("");
+                    result[2] = "1";
+                    result[5] = "1";
+                    addBlackDots(result.join(""));
+                } else if ("0" <= capital && capital <= "9") {
+                    // So like 0 through 9 are A-J but with a number sign at the beginning.
+                    // So turn on "number mode" if it's not set and start doing the numbers.
+                    // Other stuff turns number mode off ~ wait.
+                    // Except , is also valid with numbers. Oh hell. You know what?
+                    // I bet this is because of French numbers and that's the decimal point.
+                    // English 123,456.789 is French 123 456,789.
+                    // -_- shit the directions on the web site don't say how to handle this in English.
+                    // Looking at the web site my guess is that , and . are used in English braille like in English.
+                    if (!numberMode) {
+                        addBlackDots("001111");
+                        numberMode = true;
+                    }
+                    code = String.fromCharCode(capital.charCodeAt(0) + 17);
+                    addBlackDots(convert[code]);
+                } else if (letter === "." || letter === ",") {
+                    // Look at the number comments why this is special.
+                    // I'm not turning off number mode guessing that , and . work the same as in English numbers.
+                    addBlackDots(convert[letter]);
+                } else if (convert[letter]) {
+                    // Everything else turn off the number mode.
+                    numberMode = false;
+                    addBlackDots(convert[letter]);
+                }
+            }
+        }
+
+        $scope.$watch('data.text', function () {
+            textToBraille();
+        });
 
 
         return this;
